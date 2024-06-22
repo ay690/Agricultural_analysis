d5cb5839024fb815d5819d336b382c2927b7a626
Aniket Yadav <aniketyadav690@gmail.com>
2024-06-22 14:14:35 +0530

    feat: pagination added

--- src/components/TableByCrop.js
+++ src/components/TableByCrop.js
@@ -1,35 +1,65 @@
-import React from "react";
-import { Table } from "@mantine/core";
+import React, { useState } from "react";
+import { Table, Pagination } from "@mantine/core";
 import { getAverageDataByCrop } from "../helper/utils";
 
 const TableByCrop = () => {
   const data = getAverageDataByCrop();
+  const rowsPerPage = 10;
+  const [activePage, setActivePage] = useState(1);
+
+  const startIndex = (activePage - 1) * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+  const paginatedData = data.slice(startIndex, endIndex);
 
   return (
-    <Table
-      withColumnBorders
-      withTableBorder
-      withRowBorders
-      striped
-      highlightOnHover
-    >
-      <Table.Thead >
-        <Table.Tr>
-          <Table.Th style={{textAlign: "center"}}>Crop</Table.Th>
-          <Table.Th style={{textAlign: "center"}}>Average Yield (Kg/Ha)</Table.Th>
-          <Table.Th style={{textAlign: "center"}}>Average Cultivation Area (Ha)</Table.Th>
-        </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>
-        {data?.map((item, index) => (
-          <Table.Tr key={index}>
-            <Table.Td style={{textAlign: "center"}}>{item.Crop}</Table.Td>
-            <Table.Td style={{textAlign: "center"}}>{item.AverageYield}</Table.Td>
-            <Table.Td style={{textAlign: "center"}}>{item.AverageArea}</Table.Td>
+    <>
+      <Table
+        withColumnBorders
+        withTableBorder
+        withRowBorders
+        striped
+        highlightOnHover
+      >
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th style={{ textAlign: "center" }}>Crop</Table.Th>
+            <Table.Th style={{ textAlign: "center" }}>
+              Average Yield (Kg/Ha)
+            </Table.Th>
+            <Table.Th style={{ textAlign: "center" }}>
+              Average Cultivation Area (Ha)
+            </Table.Th>
           </Table.Tr>
-        ))}
-      </Table.Tbody>
-    </Table>
+        </Table.Thead>
+        <Table.Tbody>
+          {paginatedData?.map((item, index) => (
+            <Table.Tr key={index}>
+              <Table.Td style={{ textAlign: "center" }}>{item.Crop}</Table.Td>
+              <Table.Td style={{ textAlign: "center" }}>
+                {item.AverageYield}
+              </Table.Td>
+              <Table.Td style={{ textAlign: "center" }}>
+                {item.AverageArea}
+              </Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+      <div
+        style={{
+          marginTop: 20,
+          display: "flex",
+          alignItems: "end",
+          justifyContent: "end",
+        }}
+      >
+        <Pagination
+          page={activePage}
+          onChange={setActivePage}
+          total={Math.ceil(data.length / rowsPerPage)}
+        />
+      </div>
+    </>
   );
 };
 
--- src/components/TableByYear.js
+++ src/components/TableByYear.js
@@ -1,39 +1,65 @@
-import React from "react";
-import { Table } from "@mantine/core";
+import React, { useState } from "react";
+import { Table, Pagination } from "@mantine/core";
 import { getAggregatedDataByYear } from "../helper/utils";
 
 const TableByYear = () => {
   const data = getAggregatedDataByYear();
+  const rowsPerPage = 10; // Number of rows per page
+  const [activePage, setActivePage] = useState(1);
+
+  const startIndex = (activePage - 1) * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+  const paginatedData = data.slice(startIndex, endIndex);
 
   return (
-    <Table
-      withColumnBorders
-      withTableBorder
-      withRowBorders
-      striped
-      highlightOnHover
-    >
-      <Table.Thead>
-        <Table.Tr>
-          <Table.Th style={{ textAlign: "center" }}>Year</Table.Th>
-          <Table.Th style={{ textAlign: "center" }}>
-            Crop with Maximum Production
-          </Table.Th>
-          <Table.Th style={{ textAlign: "center" }}>
-            Crop with Minimum Production
-          </Table.Th>
-        </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>
-        {data?.map((item, index) => (
-          <Table.Tr key={index}>
-            <Table.Td style={{ textAlign: "center" }}>{item.Year}</Table.Td>
-            <Table.Td style={{ textAlign: "center" }}>{item.CropMax}</Table.Td>
-            <Table.Td style={{ textAlign: "center" }}>{item.CropMin}</Table.Td>
+    <>
+      <Table
+        withColumnBorders
+        withTableBorder
+        withRowBorders
+        striped
+        highlightOnHover
+      >
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th style={{ textAlign: "center" }}>Year</Table.Th>
+            <Table.Th style={{ textAlign: "center" }}>
+              Crop with Maximum Production
+            </Table.Th>
+            <Table.Th style={{ textAlign: "center" }}>
+              Crop with Minimum Production
+            </Table.Th>
           </Table.Tr>
-        ))}
-      </Table.Tbody>
-    </Table>
+        </Table.Thead>
+        <Table.Tbody>
+          {paginatedData?.map((item, index) => (
+            <Table.Tr key={index}>
+              <Table.Td style={{ textAlign: "center" }}>{item.Year}</Table.Td>
+              <Table.Td style={{ textAlign: "center" }}>
+                {item.CropMax}
+              </Table.Td>
+              <Table.Td style={{ textAlign: "center" }}>
+                {item.CropMin}
+              </Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+      <div
+        style={{
+          marginTop: 20,
+          display: "flex",
+          alignItems: "end",
+          justifyContent: "end",
+        }}
+      >
+        <Pagination
+          page={activePage}
+          onChange={setActivePage}
+          total={Math.ceil(data.length / rowsPerPage)}
+        />
+      </div>
+    </>
   );
 };
 
